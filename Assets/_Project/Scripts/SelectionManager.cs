e3ea34fc1c4d57d5e5f067b57077f0dcd96db1ef
Vlad <tomhanter785@gmail.com>
2023-03-12 13:00:27 +0300

    refactoring code

--- Assets/_Project/Scripts/SelectionManager.cs
+++ Assets/_Project/Scripts/SelectionManager.cs
@@ -1,34 +1,27 @@
-﻿﻿using UnityEngine;
+﻿using UnityEngine;
 
 public class SelectionManager : MonoBehaviour
 {
-    [SerializeField] private string selectableTag = "Selectable";
-
+    private IRayProvider _rayProvider;
+    private ISelector _selector;
     private ISelectionResponse _selectionResponse;
 
-    private Transform _selection;
-    
+    private Transform _currentSelection;
+
     private void Awake()
     {
+        _rayProvider = GetComponent<IRayProvider>();
+        _selector = GetComponent<ISelector>();
         _selectionResponse = GetComponent<ISelectionResponse>();
     }
 
     private void Update()
     {
-        if (_selection != null) _selectionResponse.OnDeselect(_selection);
+        if (_currentSelection != null) _selectionResponse.OnDeselect(_currentSelection);
 
-        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        _selector.Check(_rayProvider.CreateRay());
+        _currentSelection = _selector.GetSelection();
         
-        _selection = null;
-        if (Physics.Raycast(ray, out var hit))
-        {
-            var selection = hit.transform;
-            if (selection.CompareTag(selectableTag))
-            {
-                _selection = selection;
-            }
-        }
-
-        if (_selection != null) _selectionResponse.OnSelect(_selection);
+        if (_currentSelection != null) _selectionResponse.OnSelect(_currentSelection);
     }
 }
